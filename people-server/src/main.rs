3ae6040e948ec482a507a405fe95306344a74bd1
Tim <timothyhollabaugh@gmail.com>
2018-12-12 03:09:43 +0000

    remove /api/v1 prefix from urls. Now handled by apache.

--- people-server/src/main.rs
+++ people-server/src/main.rs
@@ -12,19 +12,20 @@ fn main() {
 
 fn handler(request: &Request) -> Response {
     router!(request,
-            (POST) ["/api/v1/users"] => {
+            (POST) ["/users"] => {
                 Response::text("501: Not implemented").with_status_code(501)
             },
-            (GET) ["/api/v1/users"] => {
+            (GET) ["/users"] => {
                 Response::text("501: Not implemented").with_status_code(501)
             },
-            (PUT) ["/api/v1/users"] => {
+            (PUT) ["/users"] => {
                 Response::text("501: Not implemented").with_status_code(501)
             },
-            (DELETE) ["/api/v1/users"] => {
+            (DELETE) ["/users"] => {
                 Response::text("501: Not implemented").with_status_code(501)
             },
             _ => {
+                eprintln!("not found: {:?}", request);
                 Response::text("404: Not found").with_status_code(404)
             }
            )
